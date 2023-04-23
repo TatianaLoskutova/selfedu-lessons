a73b1a492f4e8c3f38a49761e3b6191325d6aa5a
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-04-23 20:22:08 +0300

    final version

--- ex1.js
+++ ex1.js
@@ -1,2 +1,16 @@
-let color;
+let color = "hello"; // перый способ присваивания
 console.log(color);
+
+let message;
+message = "hi"; // вотрой способ присваивания
+console.log(message);
+
+let user = "Alex",
+  age = 25,
+  email = "1@gmail.com";
+
+age = 17; // теперь age переписан на 17
+console.log(age);
+
+const PI = 3.1415;
+console.log(PI);
